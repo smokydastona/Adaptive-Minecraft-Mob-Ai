d21291085573d328a08da0e1823e1c22c1cddac3
smokydstona <smokydstona@gmail.com>
2026-01-20 17:27:20 -0800

    Fix missing import in Forge config UI

--- src/main/java/com/minecraft/gancity/client/config/AdaptiveMobAiLoadoutConfigScreen.java
+++ src/main/java/com/minecraft/gancity/client/config/AdaptiveMobAiLoadoutConfigScreen.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.ForgeRegistries;
 
